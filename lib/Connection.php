71462151b3c3b5604de0313661fc040a3f4b85b1
Diego Leite <diegojleite@gmail.com>
2014-06-07 16:22:04 -0300

    Criada a classe de conexão com o banco de dados

---
+++ lib/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Project\Lib;
+
+class Connection
+{
+	private $host = "172.31.6.165";
+	private $db = "desafio";
+	private $user = "root";
+	private $passwd = "d13g0xD";
+
+	public function __construct(){
+
+	}
+
+	$pdo = new PDO("mysql:host=$host;dbname=$db", "$user", "$passwd");
+	$statement = $pdo->query("SELECT * FROM Usuario");
+	$row = $statement->fetch
+}
